1f7df1657b34a33fc8d6def9c40e3f193d003370
ambiguousname <ambiguousemails@proton.me>
2025-11-16 14:38:21 -0800

    Fix FormatterFactory creation, expand definition

--- src/formatter.cpp
+++ src/formatter.cpp
@@ -1,6 +1,26 @@
 #include "formatter.h"
 
 icu::message2::Formatter* FormatterFactory::createFormatter(const icu::Locale& locale, UErrorCode& status) {
-	status = U_MF_FORMATTING_ERROR;
+	String str_locale = locale.getName();
+	Formatter* f = create_formatter(str_locale);
+	if (f == nullptr) {
+		status = U_MF_FORMATTING_ERROR;
+		return nullptr;
+	} else {
+		status = U_ZERO_ERROR;
+		// return f;
+		return nullptr;
+	}
+}
+
+Formatter* FormatterFactory::create_formatter(String locale) {
 	return nullptr;
+}
+
+void FormatterFactory::_bind_methods() {
+	// BIND_VIRTUAL_METHOD(FormatterFactory, create_formatter, 0);
+}
+
+void Formatter::_bind_methods() {
+
 }
--- src/formatter.h
+++ src/formatter.h
@@ -8,18 +8,41 @@
 
 using namespace godot;
 
+// UMemory (parent class of FormatterFactory) redefines void* operator new, so we re-re-define it again here (just copying godot-cpp's definitions):
+#define OVERRIDE_ALLOC() \
+static void* operator new(std::size_t p_size, const char*, const char*) { \
+	return godot::Memory::alloc_static(p_size); \
+} \
+static void *operator new(size_t p_size, const char *p_dummy, void *(*p_allocfunc)(size_t p_size)) { \
+	return p_allocfunc(p_size); \
+} \
+static _ALWAYS_INLINE_ void *operator new(size_t p_size, const char *p_dummy, void *p_pointer, size_t check, const char *p_description) { \
+	return p_pointer; \
+}
+
 class Formatter;
 class FormatterFactory : public RefCounted, public icu::message2::FormatterFactory {
 	GDCLASS(FormatterFactory, RefCounted)
 	protected:
-		virtual icu::message2::Formatter* createFormatter(const icu::Locale& locale, UErrorCode& status) override;
+		icu::message2::Formatter* createFormatter(const icu::Locale& locale, UErrorCode& status) override;
 
 	public:
-		// Formatter* create_formatter();
-		// static void _bind_methods();
+		OVERRIDE_ALLOC()
+
+		FormatterFactory() = default;
+		~FormatterFactory() override = default;
+
+		virtual Formatter* create_formatter(String locale);
+		static void _bind_methods();
 };
 
-class Formatter : public RefCounted, public icu::message2::Formatter {
+class Formatter : public RefCounted, icu::message2::Formatter {
+	GDCLASS(Formatter, RefCounted)
 	public:
-		// static void _bind_methods();
+		OVERRIDE_ALLOC()
+
+		Formatter() = default;
+		~Formatter() override = default;
+
+		static void _bind_methods();
 };
