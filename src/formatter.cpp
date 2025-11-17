b247eca3813c77151a5b071e13c97d5e2ba0bcbb
ambiguousname <ambiguousemails@proton.me>
2025-11-17 13:35:47 -0800

    Reference/unreference created Formatters

--- src/formatter.cpp
+++ src/formatter.cpp
@@ -3,13 +3,24 @@
 icu::message2::Formatter* FormatterFactory::createFormatter(const icu::Locale& locale, UErrorCode& status) {
 	String str_locale = locale.getName();
 	Formatter* f = create_formatter(str_locale);
+
 	if (f == nullptr) {
 		status = U_MF_FORMATTING_ERROR;
 		return nullptr;
 	} else {
+		// ICU will need this Formatter* ptr for as long as FormatterFactory is attached to an MFFunctionRegistry. 
+		// To simplify, we just reference until ~FormatterFactory:
+		f->reference();
+		created_formatters.push_back(f);
+
 		status = U_ZERO_ERROR;
-		// return f;
-		return nullptr;
+		return f;
+	}
+}
+
+FormatterFactory::~FormatterFactory() {
+	for (auto f : created_formatters) {
+		f->unreference();
 	}
 }
 
@@ -18,7 +29,7 @@ Formatter* FormatterFactory::create_formatter(String locale) {
 }
 
 void FormatterFactory::_bind_methods() {
-	// BIND_VIRTUAL_METHOD(FormatterFactory, create_formatter, 0);
+	BIND_VIRTUAL_METHOD(FormatterFactory, create_formatter, 0);
 }
 
 void Formatter::_bind_methods() {
--- src/formatter.h
+++ src/formatter.h
@@ -8,7 +8,8 @@
 
 using namespace godot;
 
-// UMemory (parent class of FormatterFactory) redefines void* operator new, so we re-re-define it again here (just copying godot-cpp's definitions):
+// UMemory (parent class of FormatterFactory) redefines void* operator new, so we re-re-define it again here (just copying godot-cpp's definitions).
+// This only works because FormatterFactory and Formatter are both virtual classes, so ICU expects us to handle their allocation.
 #define OVERRIDE_ALLOC() \
 static void* operator new(std::size_t p_size, const char*, const char*) { \
 	return godot::Memory::alloc_static(p_size); \
@@ -26,17 +27,19 @@ class FormatterFactory : public RefCounted, public icu::message2::FormatterFacto
 	protected:
 		icu::message2::Formatter* createFormatter(const icu::Locale& locale, UErrorCode& status) override;
 
+		std::vector<Formatter*> created_formatters;
+
 	public:
 		OVERRIDE_ALLOC()
 
 		FormatterFactory() = default;
-		~FormatterFactory() override = default;
+		~FormatterFactory() override;
 
 		virtual Formatter* create_formatter(String locale);
 		static void _bind_methods();
 };
 
-class Formatter : public RefCounted, icu::message2::Formatter {
+class Formatter : public RefCounted, public icu::message2::Formatter {
 	GDCLASS(Formatter, RefCounted)
 	public:
 		OVERRIDE_ALLOC()
